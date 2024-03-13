5becd28b40897eb8b6eabd1086972cf143ecee92
Ayisha Alli <alli.ayisha1@gmail.com>
2024-03-13 17:56:24 +0000

    update

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-export DATABASE_URL="postgresql://postgres:@localhost:5432/casting_agency"
+export DATABASE_URL="postgres://postgresuser:9OSEiShjV4BsSByBYKQcwrjXv8GW8Kxm@dpg-cnoue8vsc6pc73d5u73g-a.oregon-postgres.render.com/casting_agency_dwu0"
 export FLASK_APP=app
 export FLASK_ENV=development
 export AUTH0_DOMAIN='dev-qhsu4ty5ej1veb8a.us.auth0.com'
@@ -6,5 +6,6 @@ export AUTH0_CLIENT_ID="weGnjYCNVNMuILJ5mE9wWqxfrZ4WC4Cz"
 export AUTH0_CLIENT_SECRET="1WrfNCMJTv0pP8D1KNMjMZG2iPQAPRhXZA82rMVtN7oCOnKaoUCUSTVCvjnZHOzH"
 export ALGORITHMS=['RS256']
 export API_AUDIENCE='https://api.bluecasting.com'
+
 
 
